749de05045e6fb614ef38785257ad9712c303560
naiaramortari <130520832+naiaramortari@users.noreply.github.com>
2023-07-17 22:42:49 -0300

    Add files via upload

---
+++ first-day.js
@@ -0,0 +1,46 @@
+telefone = "99692-2995";
+contato = "Naiara";
+
+if (telefone == "99692-2995" && contato != "Naiara"){
+   // console.log ("Confirmado")
+}
+else {
+    //console.log ("Contato Inválido");
+}
+
+
+telefone = "99692-2995";
+contato = "Naiara";
+
+if (telefone == "99692-2995" && contato == "Naiara"){
+    //console.log ("Confirmado");
+} else {
+   //console.log ("Contato Inválido");
+}
+
+numeroUm = 1;
+stringUm = "1";
+
+if(numeroUm == 1 && stringUm == "1"){
+   // console.log(numeroUm === stringUm);
+} else {
+   // console.log(numeroUm == stringUm);
+}
+
+apartamentoNaiara = "603";
+apartamento = "";
+
+if (apartamentoNaiara == "603"){
+    //console.log (apartamento = "Correto");
+} else {
+    //console.log (apartamento = "Endereço Inválido");
+}
+
+naiara = "33";
+carolina = "32";
+
+if (naiara > carolina){
+    console.log (naiara = "Mais Velha");
+} else {
+    console.log (naiara = "Mais Nova");
+}
